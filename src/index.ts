fc3a990b9244ed3fccf5f58d5da14a75e1039c7d
Mr Coxall <mr.coxall@mths.ca>
2022-09-12 15:49:05 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * By:      Mr. Coxall
+ * Version: 1.0
+ * Since:   2020-01-01
+ */
+
+console.log('Hello, World!')
+
+console.log('\nDone.')
